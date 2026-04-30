06aa2e5e6f368f4b031f5ee159517fc6eea4f24e
micha <michaelozer2003@gmail.com>
2026-04-30 13:48:06 +0300

    second commit added a db and models

---
+++ app/Models/Users.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
