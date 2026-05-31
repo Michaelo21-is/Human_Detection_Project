da63601c4bed7b45395641c4c18f620ada0d4557
micha <michaelozer2003@gmail.com>
2026-05-31 10:01:57 +0300

    added my url to the origin

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://human-detection-front.vercel.app"
+        "https://michael-site.com",
 
     ],
     allow_credentials=True,
